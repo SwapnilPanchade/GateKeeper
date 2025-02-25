f6bc1ee6ec8ed4e9e9138ebc3f5d0392ddb07cb5
SwapnilPanchade <sspanchade113@gmail.com>
2025-02-25 14:47:25 +0530

    simplified the roles of the user using enums of roles

--- src/main/java/com/example/SimplifiedRbacApplication.java
+++ src/main/java/com/example/SimplifiedRbacApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimplifiedRbacApplication {
+	//! This project is the role based online course platform for students to buy course and for admin to manage course like the access in being also managed
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimplifiedRbacApplication.class, args);
